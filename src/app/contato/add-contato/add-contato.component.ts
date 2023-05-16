bed525b8f3f2904508e947debd4f141201e5df33
Julian Alves <julianalvesinstinto@gmail.com>
2023-05-16 15:09:21 -0300

    sim

--- src/app/contato/add-contato/add-contato.component.ts
+++ src/app/contato/add-contato/add-contato.component.ts
@@ -8,13 +8,15 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['./add-contato.component.css']
 })
 export class AddContatoComponent {
-  constructor(public db: AngularFireDatabase) {
-
-
-
-  }
+  constructor(public db: AngularFireDatabase) {}
 
   addContato() {
+
+    const objeto = {
+      matricula: 1,
+      nome: "julian",
+      idade: 16
+    }
 
     this.db.list("contatos").push("contatos")
 
@@ -24,5 +26,10 @@ export class AddContatoComponent {
 
     );
 
+    this.db.object("objeto").set(objeto)
+    .then( result =>
+      console.log(result)
+
+      );
   }
 }
